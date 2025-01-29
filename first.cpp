41992a22704ba9fc93e5709a0ea4838d8c7639cd
Md Shafatullah <155663678+MRcoder011@users.noreply.github.com>
2025-01-30 02:04:38 +0530

    chlega

---
+++ first.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+
+    // Taking user input
+    int num;
+    std::cout << "Enter a number: ";
+    std::cin >> num;
+
+    // Printing the number entered by the user
+    std::cout << "You entered: " << num << std::endl;
+
+    return 0;
+}
