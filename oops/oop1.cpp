26af24d05e5c5accdba7e82b00e4c99946c386d2
Md Shafatullah <155663678+MRcoder011@users.noreply.github.com>
2025-02-06 17:18:20 +0530

    add

--- oops/oop1.cpp
+++ oops/oop1.cpp
@@ -1,13 +1,23 @@
 #include <iostream>
 using namespace std;
-int x = 2;
+
+int x = 2;  // Global variable
+
+void fun() {
+    int x = 60;          // Local variable inside fun()
+    cout << x << endl;
+    :: x = 40 ;   // Prints local x (60)
+    cout << ::x << endl;  // Accesses global x using scope resolution (::)
+}
 
 int main() { 
-    ::x = 4;
-    int  x = 20;
+    ::x = 4;             // Modifying the global x to 4
+    int x = 20;          // Local variable inside main()
 
-cout<< x<< endl;
-cout << :: x << endl;
+    cout << x << endl;    // Prints local x (20)
+    cout << ::x << endl;  // Prints modified global x (4)
 
-    return 0;
+    fun();                // Calling the fun() function
+
+    return 0;             // Properly placed return statement
 }
