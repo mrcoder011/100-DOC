6a8195b3c34ec438be8eacd7dda2c0fd9674051f
Md Shafatullah <155663678+MRcoder011@users.noreply.github.com>
2025-02-06 23:05:09 +0530

    summit

--- oops/oop1.cpp
+++ oops/oop1.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int x = 2;  // Global variable
+/*int x = 2;  // Global variable
 
 void fun() {
     int x = 60;          // Local variable inside fun()
@@ -21,3 +21,22 @@ int main() {
 
     return 0;             // Properly placed return statement
 }
+ // so in this we learned that most local will be printed first then global 
+
+*/
+ //class
+ class student{
+    /*int id;
+    int age;
+    string name ;
+    int nos;
+
+    void study(){
+        cout<< "studying"<< endl;
+
+    }*/
+ } ;
+ int main (){
+    cout<< sizeof(student)<< endl;
+
+ }
