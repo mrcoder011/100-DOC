a234a9ddada83e3ddca7d198e0e87c1e2c2bb4af
Md Shafatullah <155663678+MRcoder011@users.noreply.github.com>
2025-02-12 19:22:35 +0530

    adding

--- Linkedlist/1.cpp
+++ Linkedlist/1.cpp
@@ -7,17 +7,47 @@ public:
     node *next;
 
     node() {        // Default constructor
-        cout << "I am inside this ctor" << endl;
+     //   cout << "I am inside this default ctor" << endl;
         this->next = NULL;
     }
 
     node(int data) {   // Parameterized constructor
+       // cout << "I am inside this default ctor" << endl;
         this->data = data;
         this->next = NULL;
     }
 };
+void printLL(node*head){
+    node*temp = head;
+    while (temp != NULL){
+        cout<< temp->data<< " ->";
+        temp = temp ->next;
+
+    }
+cout<< endl;
+
+}
 
 int main() {
-    node a;   // This will call the default constructor
+   // node a;   // This will call the default constructor
+    //return 0;
+    node * first  = new node(10);
+    node * second  = new node(20);
+    node * third  = new node(30);
+    node * fourth = new node(40);
+    node * fifth  = new node(50); 
+
+    first -> next = second ;
+    second -> next = third;
+    third -> next = fourth;
+    fourth -> next = fifth;
+    node*head = first;
+
+cout<< "printing the entire  LL : "<< endl;
+printLL(head);
+
+
     return 0;
+
 }
+
