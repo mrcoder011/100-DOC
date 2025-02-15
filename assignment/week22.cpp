b82bf36e8f716e70f09fc54c297b6e07f85c7ada
Md Shafatullah <155663678+MRcoder011@users.noreply.github.com>
2025-02-15 20:31:24 +0530

    pushing

---
+++ assignment/week22.cpp
@@ -0,0 +1,22 @@
+class Solution {
+    public:
+        int pivotIndex(vector<int>& nums) {
+            int totalSum = 0, leftSum = 0;
+            
+            // Compute total sum of the array
+            for (int num : nums) {
+                totalSum += num;
+            }
+            
+            // Iterate through the array to find the pivot index
+            for (int i = 0; i < nums.size(); i++) {
+                if (leftSum == totalSum - leftSum - nums[i]) {
+                    return i;  // Found pivot index
+                }
+                leftSum += nums[i];  // Update left sum
+            }
+            
+            return -1;  // No pivot index found
+        }
+    };
+    
