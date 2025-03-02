e91e6865c17cb40cc52c4e639d02ee394660b10e
Md Shafatullah <155663678+MRcoder011@users.noreply.github.com>
2025-03-02 23:53:10 +0530

    pushing

--- Linkedlist/LL5.cpp
+++ Linkedlist/LL5.cpp
@@ -1,44 +1,46 @@
-#include <iostream>
-using namespace std;
-
-class Node {
-public:
-Node* slow = head;
-Node* fast = head ;
-while (fast != NULL){
-    fast = fast -> next;
-    if(fast != NULL){
+/**
+ * Definition for singly-linked list.
+ * struct ListListListNode {
+ *     int val;
+ *     ListListListNode *next;
+ *     ListListListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+    public:
+        ListNode *detectCycle(ListNode *head) {
+            ListNode* slow = head;
+    ListNode* fast = head ;
+    while (fast != NULL){
         fast = fast -> next;
-        slow = slow->next ;
-
+        if(fast != NULL){
+            fast = fast -> next;
+            slow = slow->next ;
+    
+        }
+        if(fast == slow){
+            break;
+    
+        }
     }
-    if(fast == slow){
-        break;
-
+    
+    // jab koi cycle nhi hoga fast null pr aajaega 
+    if (fast == NULL ){
+        return NULL;
+    
     }
-}
-// yha phucha iska mtlb loop present;
-// MAIN ALOGORITHM AND LOGIC;
-
-slow = head ;
-while (fast != slow ){
-    slow = slow -> next ; 
-    fast = fast-> next;
-
-}
-return slow ;
-
-
-}
-int main() {
-   Node *head = NULL;
-   Node *tail = NULL;
-
-   // Insert elements at the head
-   insertAtHead(head, tail, 40);
-   insertAtHead(head, tail, 30);
-   insertAtHead(head, tail, 20);
-   insertAtHead(head, tail, 10);
-   insertAtTail(head , tail , 77);
-   tail -> next = head ->next->next;
-}
+    // yha phucha iska mtlb loop present;
+    // MAIN ALOGORITHM AND LOGIC;
+    
+    slow = head ;
+    while (fast != slow ){
+        slow = slow -> next ; 
+        fast = fast-> next;
+    
+    }
+    return slow ;
+    
+    
+            
+        }
+    };
