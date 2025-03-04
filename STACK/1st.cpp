d83835dff705816190e8e6cccf33d60df77cf65e
Md Shafatullah <155663678+MRcoder011@users.noreply.github.com>
2025-03-05 00:48:54 +0530

    pushing

---
+++ STACK/1st.cpp
@@ -0,0 +1,14 @@
+ #include <iostream>
+ #include <stack>
+ using namespace std;
+
+ int main(){
+stack<int> st;
+st.push(10);
+st.push(20);
+
+cout << "size of stack ->"  << st.size()<<endl;
+
+    return 0;
+
+ }
