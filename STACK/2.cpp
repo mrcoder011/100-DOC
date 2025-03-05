ec7686c6dc8292afea419843325a5a0fb81a5df3
Md Shafatullah <155663678+MRcoder011@users.noreply.github.com>
2025-03-06 01:33:35 +0530

    stack pushing

--- STACK/2.cpp
+++ STACK/2.cpp
@@ -1,2 +1,56 @@
 #include <iostream>
-#include <>
+using namespace std;
+
+class stack{
+    public:
+    int *arr;
+    int size;
+    int top ;
+
+stack(int size ){
+    arr = new int[size];
+    this->size = size ;
+    this->top = -1;
+
+}
+void push(int data){
+    top++;
+    arr[top] = data;
+     
+}
+void pop(){
+    top--;
+
+}
+bool isempty(){
+    if(top == -1){
+        return true;
+
+    }
+    else{
+        return false;
+
+    }
+}
+int gettop(){
+    if(top == -1){
+        cout<< "no element stack empty  "<< endl;
+
+    }else{
+return arr[top];
+    }
+    
+
+}
+int getsize(){
+return top+1;
+
+}
+
+};
+int main(){
+
+
+    return 0;
+
+}
