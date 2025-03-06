abb9d381e06d9399b227bfe20ea737ce5488d014
Md Shafatullah <155663678+MRcoder011@users.noreply.github.com>
2025-03-06 23:00:58 +0530

    stack pushing

--- STACK/2.cpp
+++ STACK/2.cpp
@@ -1,79 +1,74 @@
 #include <iostream>
 using namespace std;
 
-class stack{
-    public:
-    int *arr;
+class Stack {
+public:
+    int* arr;
     int size;
-    int top ;
+    int top;
 
-stack(int size ){
-    arr = new int[size];
-    this->size = size ;
-    this->top = -1;
-
-}
-void push(int data){
-    if (top == size -1){
-        cout<< "stack completly full OVERFLOW "<< endl;
-
-
+    Stack(int size) {
+        arr = new int[size];
+        this->size = size;
+        this->top = -1;
     }
-    else{
+
+    void push(int data) {
+        if (top == size - 1) {
+            cout << "Stack completely full! OVERFLOW" << endl;
+            return;
+        }
         top++;
         arr[top] = data;
-         
     }
-   
-}
-void pop(){
-     if(top == -1){
-        cout<< "no element stack empty  "<< endl;
 
-     }
-     else{
+    void pop() {
+        if (top == -1) {
+            cout << "No element! Stack empty." << endl;
+            return;
+        }
         top--;
-     }
-
-}
-bool isempty(){
-    if(top == -1){
-        return true;
-
     }
-    else{
-        return false;
 
+    bool isempty() {
+        return (top == -1);
     }
-}
-int gettop(){
-    if(top == -1){
-        cout<< "no element stack empty  "<< endl;
 
-    }else{
-return arr[top];
+    int gettop() {
+        if (top == -1) {
+            cout << "No element! Stack empty." << endl;
+            return -1;
+        }
+        return arr[top];
     }
-    
 
-}
-int getsize(){
-return top+1;
-
-}
-void print(){
-    cout<< "stack:";
-    for (int i = 0 ; i <getsize(); i++){
-        cout<< arr[i] << endl;
-        cout<< endl;
-
+    int getsize() {
+        return top + 1;
     }
-}
 
+    void print() {
+        cout << "Top Index: " << top << endl;
+        cout << "Top Element: " << gettop() << endl;
+        cout << "Stack: ";
+        for (int i = 0; i <= top; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << endl << endl;
+    }
 };
-int main(){
 
-stack st(8);
-
+int main() {
+    Stack st(8);  // Creating a stack of size 8
+    st.push(10);
+    st.print();
+    st.push(20);
+    st.print();
+    st.push(30);
+    st.print(); 
+    st.push(40);
+    st.print();
+    st.pop();
+    st.print();
+    
     return 0;
-
 }
