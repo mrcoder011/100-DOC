696c962c59b111107f217774897edcfd121ff258
Md Shafatullah <155663678+MRcoder011@users.noreply.github.com>
2025-03-09 02:11:12 +0530

    stack pushing

--- STACK/insertsort.cpp
+++ STACK/insertsort.cpp
@@ -2,59 +2,55 @@
 #include <stack>
 using namespace std;
 
-void insertsort(stack<int> &st , int element){
-if (st.empty() || element > st.top()){
-
-st.push(element);
-return;
-
-
-}
-// ek case smbhalena 
-int temp = st.top();
-st.pop();
-insertsort(st , element) ;
-// backtracking 
-
-st.push(temp);
-
-}
-void sortstack(stack<int> &st ){
-    if(st.empty()){
+void insertsort(stack<int> &st, int element) {
+    if (st.empty() || element > st.top()) {
+        st.push(element);
         return;
-
     }
+
+    // Hold the top element and recurse
     int temp = st.top();
     st.pop();
-    // rec sort  
-    sortstack(st);
+    insertsort(st, element);
 
-    // bacltracking 
-    insertsort(temp);
-    
+    // Backtracking: Restore elements
+    st.push(temp);
 }
 
+void sortstack(stack<int> &st) {
+    if (st.empty()) {
+        return;
+    }
 
-int main(){
-    stack<int> st;
-
-st.push(10);
-st.push(7);
-st.push(12);
-
-st.push(5);
-st.push(11);
-//int element = 400;
-insertsort(st , 35) ;
-
-while(!st.empty()){
-    cout<< st.top() << " ";
+    int temp = st.top();
     st.pop();
 
+    // Recursively sort the stack
+    sortstack(st);
+
+    // Insert the removed element at the correct position
+    insertsort(st, temp);
 }
-return 0 ;
 
+int main() {
+    stack<int> st;
 
+    st.push(10);
+    st.push(7);
+    st.push(12);
+    st.push(5);
+    st.push(11);
 
+    // First, sort the stack
+    sortstack(st);
 
+    // Then, insert a new element
+    insertsort(st, 35);
+
+    // Print the sorted stack
+    while (!st.empty()) {
+        cout << st.top() << " ";
+        st.pop();
+    }
+    return 0;
 }
