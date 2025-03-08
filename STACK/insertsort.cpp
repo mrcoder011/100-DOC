205f987d3f176688af9f3dd4436428b65c9b2d8c
Md Shafatullah <155663678+MRcoder011@users.noreply.github.com>
2025-03-09 01:21:15 +0530

    stack pushing

--- STACK/insertsort.cpp
+++ STACK/insertsort.cpp
@@ -2,7 +2,7 @@
 #include <stack>
 using namespace std;
 
-void insertsort(stack<int> &st , int &element){
+void insertsort(stack<int> &st , int element){
 if (st.empty() || element > st.top()){
 
 st.push(element);
@@ -25,9 +25,8 @@ int main(){
 st.push(10);
 st.push(20);
 st.push(30);
-int element = 400;
-
-intsertAtbottom(st,element);
+//int element = 400;
+insertsort(st , 22) ;
 
 while(!st.empty()){
     cout<< st.top() << " ";
