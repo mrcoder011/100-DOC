9d99cb128586925abfde5947c5d67f85888bd375
Md Shafatullah <155663678+MRcoder011@users.noreply.github.com>
2025-03-09 01:06:27 +0530

    stack pushing cd

---
+++ STACK/insertsort.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+void insertsort(stack<int> &st , int &element){
+if (st.empty() || element > st.top()){
+
+st.push(element);
+return;
+
+
+}
+// ek case smbhalena 
+int temp = st.top();
+st.pop();
+insertsort(st , element) ;
+st.push(temp);
+
+}
+
+
+int main(){
+    stack<int> st;
+
+st.push(10);
+st.push(20);
+st.push(30);
+int element = 400;
+
+intsertAtbottom(st,element);
+
+while(!st.empty()){
+    cout<< st.top() << " ";
+    st.pop();
+
+}
+return 0 ;
+
+
+
+
+}
