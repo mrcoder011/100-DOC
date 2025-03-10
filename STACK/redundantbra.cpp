1eb49f44692a193747ae86865e19bf68a55d657c
Md Shafatullah <155663678+MRcoder011@users.noreply.github.com>
2025-03-11 00:39:36 +0530

    stack pushing

---
+++ STACK/redundantbra.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+bool checkRedundancy(string &str){
+    stack<char>st;
+    for (int i=0; i< str.length(); i++){
+        char ch = str[i];
+        if(ch == '('||  ch == '+'|| ch == '-'|| ch == '*'||ch == '/'||ch == '%'){
+            st.push(ch);
+
+        }
+        else if ( ch == ')'){
+            int operatorcount = 0;
+            while (! st.empty() && st.top() != '('){
+                char temp = st.top();
+                if ( temp == '+'|| temp == '-'||temp == '*'||temp == '/'){
+                    operatorcount++;
+
+                }
+                st.pop();
+
+
+            }
+            // yha pr jb stack empty ho gya hoga tb 
+            st.pop();
+            if (operatorcount == 0){
+                return true ;
+
+            }
+        }
+
+
+    }    
+    // agr mai yha tk aaya hu iska mtlb hr brack ke beech me ek bracket ke beech me  operator mila hoga ;
+    return false ;
+
+
+}
+int main(){
+    string str = "((a+b)*(c+d))";
+    bool ans = checkRedundancy(str);
+    if (ans == true ){
+        cout<< "ans is present "<< endl;
+
+    }
+    else{
+        cout<< "ans is not present "<< endl ;
+
+    }
+    return 0;
+
+}
