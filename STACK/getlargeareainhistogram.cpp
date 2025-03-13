d7e87a5b7aed008a8f6e55fe5276c17833e6f344
Md Shafatullah <155663678+MRcoder011@users.noreply.github.com>
2025-03-14 00:25:36 +0530

    expressjs pushing

---
+++ STACK/getlargeareainhistogram.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+int largestRectangleArea(vector<int>& heights) {
+    stack<int> st; // Monotonic increasing stack
+    int maxArea = 0;
+    int n = heights.size();
+
+    for (int i = 0; i <= n; i++) {
+        while (!st.empty() && (i == n || heights[i] < heights[st.top()])) {
+            int height = heights[st.top()];
+            st.pop();
+            int width = st.empty() ? i : i - st.top() - 1;
+            maxArea = max(maxArea, height * width);
+        }
+        st.push(i);
+    }
+
+    return maxArea;
+}
+
+int main() {
+    vector<int> heights = {2, 1, 5, 6, 2, 3};
+    cout << "Largest Rectangle Area: " << largestRectangleArea(heights) << endl;
+    return 0;
+}
