969e54d982f9b29ef8cd2e699c638510d0d25bb7
Md Shafatullah <155663678+MRcoder011@users.noreply.github.com>
2025-03-14 23:15:10 +0530

    Queue pushing

--- Queue/1.cpp
+++ Queue/1.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
 #include<queue>
 using namespace std;
+class queue{
+public : 
+int *arr ;
+int size ;
+int front ;
+int rear ;
+
+queue(int size ){
+    arr = new int[size];
+    this-> size = size ;
+    front = -1;
+    rear = -1;
+
+}
+void push(int val ){
+    if (rear == size -1 ){
+        cout << "Queue is full"<<endl;
+        return ;
+    }
+    else if(front == -1 && rear == -1 ){
+        front ++;
+        rear ++;
+        arr[rear] = val;
+        
+
+
+    }
+}
+
+
+};
 int main(){
     queue<int> q;  
     q.push(1);
