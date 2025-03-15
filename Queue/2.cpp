231d21a359ccacc5d6f8952e7bac5bd326bd9296
Md Shafatullah <155663678+MRcoder011@users.noreply.github.com>
2025-03-16 00:46:09 +0530

    Queue pushing

---
+++ Queue/2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+class CQueue {
+public:
+    int *arr;
+    int size;
+    int front;
+    int rear;
+    CQueue(int size ){
+        arr = new int [size];
+        this->size = size ;
+        front = -1 ;
+        rear = -1;
+
+
+    }
+    void push(int val){
+        // overflow condition
+        // empty condition
+       
+        // circular condition
+ // normal condition
+
+ if (front == 0 && rear == size -1 ){}
+    }
+}
+int main(){
+
+}
