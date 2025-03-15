1477dd63be0380ac45ffafb81350b617300f6e7e
Md Shafatullah <155663678+MRcoder011@users.noreply.github.com>
2025-03-16 01:29:57 +0530

    Queue pushing

--- Queue/2.cpp
+++ Queue/2.cpp
@@ -53,12 +53,68 @@ if (front == -1 && rear == -1){
     return;
 }
 else if (front == rear ){
-    
+    arr[front] =-1;
+    front ++;
+
+
+}else if(front == size -1){
+    arr[front] = -1;
+    front = 0;
+
+}else if {
+    arr[front] = -1;
+    front++;
+}
+    }
+    bool isempty(){
+        return (front == -1 && rear == -1);
+    }
+    int getsize(){
+        if (front == -1 && rear == -1) return 0;
+        return rear - front + 1;
+    }
+    int getfront(){
+        if (front == -1 && rear == -1){
+            cout << "Queue is empty" << endl;
+            return -1;
+        }
+        return arr[front];
+    }
+    int getrear(){
+        if (front == -1 && rear == -1){
+            cout << "Queue is empty" << endl;
+            return -1;
+        }
+        return arr[rear];
+    }
+    void print(){
+        if (isempty()){
+            cout << "Queue is empty" << endl;
+            return;
+        }
+        cout << "Queue elements: ";
+        if (front <= rear){
+            for (int i = front; i <= rear; i++){
+                cout << arr[i] << " ";
+            }
+            cout << endl;
+        }else{
+            for (int i = front; i < size; i++){
+                cout << arr[i] << " ";
+            }
+            for (int i = 0; i <= rear; i++){
+                cout << arr[i] << " ";
+            }
+            cout << endl;
 }
         //
     }
 
 }
 int main(){
+    CQueue q(5);
+  q.print();
+  return 0;
+
 
 }
