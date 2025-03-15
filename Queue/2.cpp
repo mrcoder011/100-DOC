c5ce7f2e6593fd655d11eca23f6b64490f9e5270
Md Shafatullah <155663678+MRcoder011@users.noreply.github.com>
2025-03-16 01:10:48 +0530

    Queue pushing

--- Queue/2.cpp
+++ Queue/2.cpp
@@ -26,8 +26,17 @@ public:
  if (front == 0 && rear == size -1 ){
     cout << "Queue is full" << endl;
     return;
-    
+
  }
+ else (front == -1 && rear == -1){
+     front++
+     rear++
+     arr[rear] = val;
+    }
+    else if  ( rear == size -1 && front != 0){
+        rear = 0;
+        arr[rear ] = val;
+        
     }
 }
 int main(){
