43033e11dfba947ea050985019ae899255f555ee
Md Shafatullah <155663678+MRcoder011@users.noreply.github.com>
2025-03-17 00:50:13 +0530

    Queue pushing

--- Queue/2.cpp
+++ Queue/2.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 using namespace std;
 
@@ -10,118 +9,68 @@ public:
     int rear;
 
     CQueue(int size) {
-        arr = new int[size];
         this->size = size;
+        arr = new int[size];
         front = -1;
         rear = -1;
+
+        // Initialize array with zeros
+        for (int i = 0; i < size; i++) {
+            arr[i] = 0;
+        }
     }
 
     void push(int val) {
         // Overflow condition
-        if (front == 0 && rear == size - 1) {
+        if ((front == 0 && rear == size - 1) || (rear == front - 1)) {
             cout << "Queue is full" << endl;
             return;
         }
-        
-        // Empty condition
-        else if (front == -1 && rear == -1) {
-            front++;
-            rear++;
-            arr[rear] = val;
-        }
 
+        // Empty condition
+        if (front == -1) {
+            front = rear = 0;
+        }
         // Circular condition
         else if (rear == size - 1 && front != 0) {
             rear = 0;
-            arr[rear] = val;
         }
-
         // Normal condition
         else {
             rear++;
-            arr[rear] = val;
         }
+
+        arr[rear] = val;
     }
 
-    void pop() {
-        // Underflow condition
-        if (front == -1 && rear == -1) {
-            cout << "Queue is empty" << endl;
-            return;
-        }
-
-        // If only one element is present
-        else if (front == rear) {
-            arr[front] = -1;
-            front = -1;
-            rear = -1;
-        }
-
-        // Circular condition
-        else if (front == size - 1) {
-            arr[front] = -1;
-            front = 0;
-        }
-
-        // Normal condition
-        else {
-            arr[front] = -1;
-            front++;
-        }
-    }
-
-    /*bool isempty() {
-        return (front == -1 && rear == -1);
-    }
-
-    int getsize() {
-        if (front == -1 && rear == -1) return 0;
-        return rear - front + 1;
-    }
-
-    int getfront() {
-        if (front == -1 && rear == -1) {
-            cout << "Queue is empty" << endl;
-            return -1;
-        }
-        return arr[front];
-    }
-
-    int getrear() {
-        if (front == -1 && rear == -1) {
-            cout << "Queue is empty" << endl;
-            return -1;
-        }
-        return arr[rear];
-    }
-*/
     void print() {
-       cout << "printing queue" << endl;
-       for(int i=0;i<size;i++){
-           cout << arr[i] << " ";
-
+        cout << "Printing Queue: ";
+        for (int i = 0; i < size; i++) {
+            cout << arr[i] << " ";
         }
         cout << endl;
     }
-     /* cout << "Queue elements: ";
-        if (front <= rear) {
-            for (int i = front; i <= rear; i++) {
-                cout << arr[i] << " ";
-            }
-        } else {
-            for (int i = front; i < size; i++) {
-                cout << arr[i] << " ";
-            }
-            for (int i = 0; i <= rear; i++) {
-                cout << arr[i] << " ";
-            }
-        }
-        cout << endl;
-    }*/
 };
 
 int main() {
     CQueue q(5);
-    q.print();
+    
+    q.print();  // Initially prints: 0 0 0 0 0
+
+    q.push(10);
+    q.print();  // Prints: 10 0 0 0 0
+
+    q.push(20);
+    q.print();  // Prints: 10 20 0 0 0
+
+    q.push(30);
+    q.print();  // Prints: 10 20 30 0 0
+
+    q.push(40);
+    q.print();  // Prints: 10 20 30 40 0
+
+    q.push(50);
+    q.print();  // Prints: 10 20 30 40 50
+
     return 0;
 }
