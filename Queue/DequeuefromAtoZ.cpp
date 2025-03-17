2f670b97be89a41b88524b1649fcc76be015a9a0
Md Shafatullah <155663678+MRcoder011@users.noreply.github.com>
2025-03-17 23:34:13 +0530

    Queue pushing

--- Queue/DequeuefromAtoZ.cpp
+++ Queue/DequeuefromAtoZ.cpp
@@ -16,6 +16,51 @@ class Deque {
 
     }
     void PushFront(int val){
-        
+    // Overflow condition
+    // empty condition  
+    // normal condition
+    if (front == 0 && rear == size -1 ){
+        cout << "queue is full" << endl;
+        return;
+    }
+    else if (front == -1 && rear == -1 ){
+        front++;
+        rear++;
+        arr[front] = val;
+
+
+    }
+    else {
+        if (fr)
+    }
+
+    
+
+    }
+    void PushBack(int val){
+    // Overflow condition
+    // empty condition
+    // normal condition
+    if (rear == size -1){
+        cout << "Queue is full" << endl;
+        return;
+    }
+    else if(front == -1 && rear == -1 ){
+        front++;
+        rear++;
+        arr[rear] = val;
+
+    }
+    else {
+rear++;
+arr[rear] = val;
+
+    }
+    }
+    void PopFront(){
+    
+    }
+    void PopBack(){
+    
     }
 };
