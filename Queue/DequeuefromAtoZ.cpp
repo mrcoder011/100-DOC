b474886643b3c238f98e50f4ecd3a1705bd1285f
Md Shafatullah <155663678+MRcoder011@users.noreply.github.com>
2025-03-18 00:43:34 +0530

    Queue pushing

--- Queue/DequeuefromAtoZ.cpp
+++ Queue/DequeuefromAtoZ.cpp
@@ -19,7 +19,7 @@ class Deque {
     // Overflow condition
     // empty condition  
     // normal condition
-    if (front == 0 && rear == size -1 ){
+    if (front == 0 && rear == size -1 || rear == front -1){
         cout << "queue is full" << endl;
         return;
     }
@@ -29,6 +29,12 @@ class Deque {
         arr[front] = val;
 
 
+    }
+    else if (front == 0 && rear != size -1){
+        front = size -1;
+        arr[front ] = val;
+
+        
     }
     else {
         // rear ke case me rear ko aage laate h or phir insert krte h 
@@ -56,6 +62,11 @@ class Deque {
         arr[rear] = val;
 
     }
+    else if (rear == size -1 && front != 0){
+        rear = 0;
+        arr[rear] = val;
+
+    }
     else {
 rear++;
 arr[rear] = val;
@@ -79,6 +90,57 @@ arr[rear] = val;
     
     }
     void PopBack(){
+        // Underflow condition
+//single element condition
+// normal condition 
+if (front == -1 && rear == -1 ){
+    cout<< "queue is empty" << endl;
+    return;
+}
+else if (rear ==0){
+    arr[rear] = -1;
+    rear = size -1;  
+
+}
+if (front == rear){
+    arr[rear ] = -1;
+    front = -1 ;
+    rear = -1 ;
+
     
     }
+    else 
+    {
+        arr[rear] = -1; // remove kr dia and then piche le aaya  rear ko
+
+        rear --;
+
+    }
+    else if {
+ if (front == size -1){
+    front = 0;
+    }
 };
+};
+void print(){
+    cout << "Printing Queue: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+};
+int main() {
+    Deque dq(5);
+    dq.PushBack(10);
+    dq.PushBack(20);
+    dq.PushBack(30);
+    dq.PushBack(40);
+    dq.PushBack(50);
+    dq.PushBack(60);
+    dq.PushFront(70);
+    dq.PushFront(80);
+    dq.PushFront(90);
+    dq.PushFront(100);
+    return 0;
+}
