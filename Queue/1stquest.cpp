bcdb1ae8cce30fba8d614fd3368bb24ad173a524
Md Shafatullah <155663678+MRcoder011@users.noreply.github.com>
2025-03-18 01:27:03 +0530

    Queue pushing

---
+++ Queue/1stquest.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <queue>
+#include <stack>
+using namespace std;
+void reverseQueue(queue <int > &q) {
+
+}
+int main (){
+    queue<int> q;
+    q.push(10);
+    q.push(20);
+    q.push(30);
+    q.push(40);
+    q.push(50);
+    q.push(60);
+
+    reverseQueue(q);
+    return 0;
+
+}
