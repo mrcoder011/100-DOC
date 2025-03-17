9f53d2948813e50b00cb244924579968019707c4
Md Shafatullah <155663678+MRcoder011@users.noreply.github.com>
2025-03-18 00:50:31 +0530

    Queue pushing

--- Queue/DequeuefromAtoZ.cpp
+++ Queue/DequeuefromAtoZ.cpp
@@ -2,145 +2,129 @@
 using namespace std;
 
 class Deque {   
-    public:
+public:
     int *arr;
     int size;
     int front;
     int rear;
 
-    Deque(int size){
+    Deque(int size) {
         arr = new int[size];
         this->size = size;
         front = -1;
         rear = -1;
-
-    }
-    void PushFront(int val){
-    // Overflow condition
-    // empty condition  
-    // normal condition
-    if (front == 0 && rear == size -1 || rear == front -1){
-        cout << "queue is full" << endl;
-        return;
-    }
-    else if (front == -1 && rear == -1 ){
-        front++;
-        rear++;
-        arr[front] = val;
-
-
-    }
-    else if (front == 0 && rear != size -1){
-        front = size -1;
-        arr[front ] = val;
-
-        
-    }
-    else {
-        // rear ke case me rear ko aage laate h or phir insert krte h 
-        // front ke case me front ko piche laate h or phir insert krte h
-
-        front--;
-        arr[front] = val;
-
     }
 
-    
+    void PushFront(int val) {
+        // Overflow condition
+        if ((front == 0 && rear == size - 1) || (rear == front - 1)) {
+            cout << "Queue is full" << endl;
+            return;
+        }
+        // Empty condition
+        else if (front == -1 && rear == -1) {
+            front = rear = 0;
+            arr[front] = val;
+        }
+        // Circular condition
+        else if (front == 0 && rear != size - 1) {
+            front = size - 1;
+            arr[front] = val;
+        }
+        // Normal condition
+        else {
+            front--;
+            arr[front] = val;
+        }
+    }
 
+    void PushBack(int val) {
+        // Overflow condition
+        if ((front == 0 && rear == size - 1) || (rear == front - 1)) {
+            cout << "Queue is full" << endl;
+            return;
+        }
+        // Empty condition
+        else if (front == -1 && rear == -1) {
+            front = rear = 0;
+            arr[rear] = val;
+        }
+        // Circular condition
+        else if (rear == size - 1 && front != 0) {
+            rear = 0;
+            arr[rear] = val;
+        }
+        // Normal condition
+        else {
+            rear++;
+            arr[rear] = val;
+        }
     }
-    void PushBack(int val){
-    // Overflow condition
-    // empty condition
-    // normal condition
-    if (rear == size -1){
-        cout << "Queue is full" << endl;
-        return;
-    }
-    else if(front == -1 && rear == -1 ){
-        front++;
-        rear++;
-        arr[rear] = val;
 
-    }
-    else if (rear == size -1 && front != 0){
-        rear = 0;
-        arr[rear] = val;
-
-    }
-    else {
-rear++;
-arr[rear] = val;
-
-    }
-    }
-    void PopFront(){
+    void PopFront() {
+        // Underflow condition
         if (front == -1 && rear == -1) {
             cout << "Queue is empty" << endl;
             return;
-        } 
+        }
+        // Single element condition
         else if (front == rear) {
             arr[front] = -1;
-            front = -1;
-            rear = -1;
-        } 
+            front = rear = -1;
+        }
+        // Circular condition
+        else if (front == size - 1) {
+            arr[front] = -1;
+            front = 0;
+        }
+        // Normal condition
         else {
             arr[front] = -1;
             front++;
         }
-    
     }
-    void PopBack(){
+
+    void PopBack() {
         // Underflow condition
-//single element condition
-// normal condition 
-if (front == -1 && rear == -1 ){
-    cout<< "queue is empty" << endl;
-    return;
-}
-else if (rear ==0){
-    arr[rear] = -1;
-    rear = size -1;  
-
-}
-if (front == rear){
-    arr[rear ] = -1;
-    front = -1 ;
-    rear = -1 ;
-
-    
+        if (front == -1 && rear == -1) {
+            cout << "Queue is empty" << endl;
+            return;
+        }
+        // Single element condition
+        else if (front == rear) {
+            arr[rear] = -1;
+            front = rear = -1;
+        }
+        // Circular condition
+        else if (rear == 0) {
+            arr[rear] = -1;
+            rear = size - 1;
+        }
+        // Normal condition
+        else {
+            arr[rear] = -1;
+            rear--;
+        }
     }
-    else 
-    {
-        arr[rear] = -1; // remove kr dia and then piche le aaya  rear ko
 
-        rear --;
-
-    }
-    else if {
- if (front == size -1){
-    front = 0;
+    void print() {
+        cout << "Printing Queue: ";
+        for (int i = 0; i < size; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
     }
 };
-};
-void print(){
-    cout << "Printing Queue: ";
-    for (int i = 0; i < size; i++) {
-        cout << arr[i] << " ";
-    }
-    cout << endl;
-}
-};
+
 int main() {
     Deque dq(5);
     dq.PushBack(10);
     dq.PushBack(20);
     dq.PushBack(30);
     dq.PushBack(40);
-    dq.PushBack(50);
-    dq.PushBack(60);
-    dq.PushFront(70);
-    dq.PushFront(80);
-    dq.PushFront(90);
-    dq.PushFront(100);
+   dq.PushBack(50);
+    
+    dq.print(); // Display the queue
+    
     return 0;
 }
