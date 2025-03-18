bca6c98d6a7b49b6e4c7214d196abb0deeac83ec
Md Shafatullah <155663678+MRcoder011@users.noreply.github.com>
2025-03-18 23:58:46 +0530

    Queue pushing

---
+++ Queue/Interleavefirstandsecondhalfofqueue.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <queue>
+#include <stack>
+using namespace std;
+int main(){
+    void Interleave(queue<int> &q){
+        
+    }
+queue<int> q;
+q.push(11);
+q.push(12);
+q.push(13);
+q.push(14);
+q.push(15);
+q.push(16);
+};
