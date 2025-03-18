5abe303bd026df2637a0fbbec04dcae98de9e736
Md Shafatullah <155663678+MRcoder011@users.noreply.github.com>
2025-03-18 22:07:52 +0530

    Queue pushing

---
+++ Queue/kreverse.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <queue>
+#include <stack>
+using namespace std;
+void reversefirstk( queue<int> &q, int k){
+    // push first k elewments in stack
+    // push all k element into queue
+    // pop  and push first (n-k) elements in queue again 
+
+
+}
+
+int main(){
+    q.push(10);
+    q.push(20);     
+    q.push(30);
+    q.push(40);
+    q.push(50);
+};
