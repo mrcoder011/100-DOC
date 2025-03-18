923a2b9e74ac17db4245ca306002ac4f8f3cd01d
Md Shafatullah <155663678+MRcoder011@users.noreply.github.com>
2025-03-18 21:01:49 +0530

    Queue pushing

--- Queue/1stquest.cpp
+++ Queue/1stquest.cpp
@@ -2,24 +2,35 @@
 #include <queue>
 #include <stack>
 using namespace std;
-void reverseQueue(queue <int > &q) {
+
+void reverseQueue(queue<int> &q) {
     stack<int> s;
-    // one by one q se lo stack me daalo
-    while (!q.empty()){
+    // Transfer elements from queue to stack
+    while (!q.empty()) {
         int frontelement = q.front();
         q.pop();
         s.push(frontelement);
-
     }
-/// one by one stack se lo or q me daaalop 
-while (!s.empty()){
-    int topelement = s.top();
-    s.pop();
-    q.push(topelement);
+    // Transfer elements back from stack to queue
+    while (!s.empty()) {
+        int topelement = s.top();
+        s.pop();
+        q.push(topelement);
+    }
+}
 
-}
-}
-int main (){
+void reverse(queue<int> &q) {
+    if (q.empty()) {
+        return;
+    }
+    int element = q.front();
+    q.pop(); 
+    // Recursively reverse the remaining queue
+    reverse(q);
+    q.push(element);
+};
+
+int main() {
     queue<int> q;
     q.push(10);
     q.push(20);
@@ -28,12 +39,11 @@ int main (){
     q.push(50);
     q.push(60);
 
-    reverseQueue(q);
-    cout << "Reversed Queue: "<< endl;
-    while (!q.empty()){
+    reverse(q);
+    cout << "Reversed Queue: " << endl;
+    while (!q.empty()) {
         cout << q.front() << " ";
         q.pop();
     }
     return 0;
-
-}
+};
