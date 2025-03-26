cc067b5113fd77541cf3d51c41aa63d3c8134949
Md Shafatullah <155663678+MRcoder011@users.noreply.github.com>
2025-03-26 16:39:26 +0530

    Queue pushing

---
+++ Queue/frequency.cpp
@@ -0,0 +1,36 @@
+ #include <iostream>
+ #include <queue>
+ using namespace std;
+ int main(){
+    string str = "ababc";
+    queue<char> q;
+    int freq[26] = {0};
+    for (int i=0; i< str.length(); i++)
+    {
+        char ch = str[i];
+        freq[ch - 'a']++;
+        q.push(ch);
+        // answer find answer 
+        while (!q.empty()){
+            char frontchar = q.front();
+            if(freq[frontchar - 'a'] > 1){
+                // yaani yeh answer nhi h 
+                q.pop();
+
+
+            }
+            else {
+                // == 1 vala case yaaani yhi answer h 
+                cout << frontchar << endl;
+break;
+
+            }
+
+        }
+        if(q.empty()){
+            cout << "#" << endl;
+        }
+    }
+    return 0;
+
+ }
