607baf2633e3e9058acf2fd45a76c4aae75df8c1
Md Shafatullah <155663678+MRcoder011@users.noreply.github.com>
2025-03-26 11:23:19 +0530

    Queue pushing

--- Queue/sliding.cpp
+++ Queue/sliding.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
-#include <dequeue>
-using namespace std; 
-int main (){
-void sliding(int *arr , int n ){
+#include <queue>
+using namespace std;
+void sliding(int *arr , int n , int k){
     deque<int > dq;
     // base case 
     for (int i=0; i<k; i++){
+
         int element = arr[i];
        if(element < 0){
 dq.push_back(i);
@@ -15,14 +15,42 @@ dq.push_back(i);
     }
     // process remaing windows -> removal and addition ;
     for (int i=k; i<n; i++){
+        if (dq.empty()){
+            cout << "0" << endl;
+        }
+        else{
+            cout << arr[dq.front()] << endl;
+        }
+         
+        
         cout << dq.front()<< endl;
-        
-        
-    }
-    if (n<=k){
-        return;
-
-    }
+        // remove element which are not part of window
+        // REMOVAL 
+        if(i - dq.front() >= k)
+    dq.pop_front();
 }
+
+        //ADDITION 
+        if (arr[i]< 0){
+            dq.push_back(i);
+        }
+        
+        
+    }
+if (dq.empty()){
+    cout << "0" << endl;
+}
+else{
+    cout << arr[dq.front()] << endl;
+}
+ 
+
+int main (){
+   int arr[] = {2, -5, 4, -1, -2, 0, 5}; 
+   int size = 7;
+   int k = 3;
+    sliding(arr, size, k);
+    return 0;
+
 
 }
