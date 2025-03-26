4fe288aa22c84fd05ab4a0b80358dfba09b074b8
Md Shafatullah <155663678+MRcoder011@users.noreply.github.com>
2025-03-26 13:13:13 +0530

    Queue pushing

--- Queue/sliding.cpp
+++ Queue/sliding.cpp
@@ -1,56 +1,46 @@
 #include <iostream>
 #include <queue>
 using namespace std;
-void sliding(int *arr , int n , int k){
-    deque<int > dq;
-    // base case 
-    for (int i=0; i<k; i++){
 
+void sliding(int *arr, int n, int k) {
+    deque<int> dq;
+    // base case
+    for (int i = 0; i < k; i++) {
         int element = arr[i];
-       if(element < 0){
-dq.push_back(i);
-
-       }
-        
-    }
-    // process remaing windows -> removal and addition ;
-    for (int i=k; i<n; i++){
-        if (dq.empty()){
-            cout << "0" << endl;
-        }
-        else{
-            cout << arr[dq.front()] << endl;
-        }
-         
-        
-        cout << dq.front()<< endl;
-        // remove element which are not part of window
-        // REMOVAL 
-        if(i - dq.front() >= k)
-    dq.pop_front();
-}
-
-        //ADDITION 
-        if (arr[i]< 0){
+        if (element < 0) {
             dq.push_back(i);
         }
-        
-        
     }
-if (dq.empty()){
-    cout << "0" << endl;
-}
-else{
-    cout << arr[dq.front()] << endl;
-}
- 
+    // process remaining windows -> removal and addition
+    for (int i = k; i < n; i++) {
+        if (dq.empty()) {
+            cout << "0" << endl;
+        } else {
+            cout << arr[dq.front()] << endl;
+        }
 
-int main (){
-   int arr[] = {2, -5, 4, -1, -2, 0, 5}; 
-   int size = 7;
-   int k = 3;
+        // remove element which is not part of window
+        if (i - dq.front() >= k) {
+            dq.pop_front();
+        }
+
+        // ADDITION
+        if (arr[i] < 0) {
+            dq.push_back(i);
+        }
+    }
+
+    if (dq.empty()) {
+        cout << "0" << endl;
+    } else {
+        cout << arr[dq.front()] << endl;
+    }
+}
+
+int main() {
+    int arr[] = {2, -5, 4, -1, -2, 0, 5};
+    int size = 7;
+    int k = 3;
     sliding(arr, size, k);
     return 0;
-
-
 }
