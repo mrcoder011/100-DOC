5a48cbf0a81e1c55c984349fa85b852ae7f97d11
Md Shafatullah <155663678+MRcoder011@users.noreply.github.com>
2025-03-26 10:26:23 +0530

    Queue pushing

---
+++ Queue/sliding.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <dequeue>
+using namespace std; 
+int main (){
+void sliding(int *arr , int n ){
+    deque<int > dq;
+    // base case 
+    for (int i=0; i<k; i++){
+        int element = arr[i];
+       if(element < 0){
+dq.push_back(i);
+
+       }
+        
+    }
+    // process remaing windows -> removal and addition ;
+    for (int i=k; i<n; i++){
+        cout << dq.front()<< endl;
+        
+        
+    }
+    if (n<=k){
+        return;
+
+    }
+}
+
+}
