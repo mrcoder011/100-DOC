11409099b11cd7c276d51d62609c17d25d339150
Md Shafatullah <155663678+MRcoder011@users.noreply.github.com>
2025-04-06 23:03:24 +0530

    binary tree pushing

--- Binarytree/levelorder.cpp
+++ Binarytree/levelorder.cpp
@@ -1,24 +1,67 @@
 #include <iostream>
 #include <queue>
 using namespace std;
-void levelorderTransversal(node *root){
+
+class node {
+public:
+    int data;
+    node *left;
+    node *right;
+
+    node(int val) {
+        this->data = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+// Function to create a binary tree from user input
+node* createtree() {
+    cout << "Enter data (-1 for NULL): ";
+    int data;
+    cin >> data;
+
+    if (data == -1) {
+        return NULL;
+    }
+
+    node* root = new node(data);
+
+    cout << "Enter left of " << data << ": ";
+    root->left = createtree();
+
+    cout << "Enter right of " << data << ": ";
+    root->right = createtree();
+
+    return root;
+}
+
+// Function for Level Order Traversal
+void levelorderTraversal(node* root) {
+    if (root == NULL) return;
+
     queue<node*> q;
     q.push(root);
-    while (!q.empty()){
-        node*front = q.front();
+
+    while (!q.empty()) {
+        node* front = q.front();
         q.pop();
-        cout << front -> data << " ";
-        if (front-> left == NULL){
-            q.push(front -> left);
 
-        } 
-        if (front -> right == NULL){
-            q.push(front -> right);
+        cout << front->data << " ";
 
+        if (front->left != NULL) {
+            q.push(front->left);
+        }
+
+        if (front->right != NULL) {
+            q.push(front->right);
         }
     }
 }
-int main (){
-levelorderTransversal(root);
+
+int main() {
+    node* root = createtree();
+    cout << "\nLevel Order Traversal: ";
+    levelorderTraversal(root);
     return 0;
 }
