6f19454af9c2d887ab39481b9604581825a6a253
Md Shafatullah <155663678+MRcoder011@users.noreply.github.com>
2025-04-14 23:56:59 +0530

    binary tree pushing

---
+++ assignment/movenegpos.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+void moveallnegleft(int arr[], int n){
+    
+    int  l = 0;
+    int h = n-1;
+    while (l <h){
+        if(arr[l] < 0){
+            l++;
+        }
+        else if (arr[h] >= 0){
+            h--;
+
+        }
+        else {
+            swap(arr[l], arr[h]);
+            l++;
+            h--;
+
+        }
+    }
+}
+
+int main (){
+int arr[] = {1,2,-4,-6,7};
+int n = sizeof(arr)/sizeof(int);
+moveallnegleft(arr, n);
+for (int i = 0; i < n; i++){
+    cout << arr[i] << " ";
+}
+cout << endl;
+return 0;
+ 
+}
