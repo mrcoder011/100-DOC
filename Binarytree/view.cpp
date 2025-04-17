6988db87cd75a7381353f743f9ab147d349b0de4
Md Shafatullah <155663678+MRcoder011@users.noreply.github.com>
2025-04-18 01:10:21 +0530

    binary tree pushing

---
+++ Binarytree/view.cpp
@@ -0,0 +1,19 @@
+#include <iostream> 
+using namespace std;
+class Node {
+    public:
+    int data ;
+    Node*left;
+    Node*right;
+    Node(int val){
+        this->data = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+
+
+};
+void view()
+int main(){
+
+}
