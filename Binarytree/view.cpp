5930af6bc703f98b9f24f56424c08806f5abe8bd
Md Shafatullah <155663678+MRcoder011@users.noreply.github.com>
2025-04-18 13:13:21 +0530

    binary tree pushing

--- Binarytree/view.cpp
+++ Binarytree/view.cpp
@@ -13,7 +13,47 @@ class Node {
 
 
 };
-void view()
-int main(){
+Node *CreateTree(){
+    cout << "enter the value ";
+    int value ;
+    cin >> value;
+    if(value == -1 ){
+        return NULL;  
 
+    } 
+    Node *root = new Node(value);
+    root -> left = CreateTree();
+    root-> right = CreateTree();
+    return root;
+     
+} 
+void leftview(Node*root int level , int size){
+    if(root == NULL){
+        return;
+    
+    }
+    if (level == leftview.size() ){
+       // push krlo
+       leftview.push_back(root->data);
+
+
+    }
+printleft(root->left, level+1, size);
+    printright(root->right, level+1, size);
+    return;
+
+}
+
+
+
+int main(){
+leftview.push_back(-1);
+    Node*root = CreateTree();
+    int size = 0;
+    leftview(root, 0, size);
+    cout << "left view of the tree is : ";
+    for(int i = 0; i < leftview.size(); i++){
+        cout << leftview[i] << " ";
+    }
+    return 0;
 }
