62fd969dfb1857662f4adb5f9052d0953d251323
Md Shafatullah <155663678+MRcoder011@users.noreply.github.com>
2025-04-23 22:03:05 +0530

    binary tree pushing

---
+++ Binarytree/diagtran.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <map>
+#include <queue>
+#include <vector>
+using namespace std;
+class Node {
+public:
+    int data;
+    Node *left;
+    Node *right;
+    Node(int val) {
+        this->data = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
