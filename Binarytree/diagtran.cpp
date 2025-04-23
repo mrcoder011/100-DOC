f1d80ee2b11b15952fcb6259141eab610a004aab
Md Shafatullah <155663678+MRcoder011@users.noreply.github.com>
2025-04-23 22:23:00 +0530

    binary tree pushing

--- Binarytree/diagtran.cpp
+++ Binarytree/diagtran.cpp
@@ -13,4 +13,30 @@ public:
         this->left = NULL;
         this->right = NULL;
     }
+    class Solution {
+        public:
+            vector<int> diagonal(Node* root) {
+                vector<int> ans;
+                if (!root)
+                    return ans;
+                
+                queue<Node*> q;
+                q.push(root);
+                
+                while (!q.empty()) {
+                    Node* temp = q.front();
+                    q.pop();
+                    
+                    while (temp) {
+                        ans.push_back(temp->data);
+                        if (temp->left)
+                            q.push(temp->left);
+                        temp = temp->right;
+                    }
+                }
+                
+                return ans;
+            }
+        };
+        
 };
