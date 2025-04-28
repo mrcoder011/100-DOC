66705a3f12221eddc370ab7a017fb5b52b1753c4
Md Shafatullah <155663678+MRcoder011@users.noreply.github.com>
2025-04-29 00:14:36 +0530

    assignments question

--- assignment/spiral.cpp
+++ assignment/spiral.cpp
@@ -1,47 +1,47 @@
 class Solution {
     public:
         vector<int> spiralOrder(vector<vector<int>>& matrix) {
-            vector<int>ans;
-             int m = matrix.size();
-             int n = matrix[0].size();
-             int totalelement = m*n;
-              
-             int startcol = 0;
-             int endcol = n-1;
-             int satrtrow = 0;
-             int endrow = m-1;
-             int count = 0 ;
+            vector<int> ans;
+            int m = matrix.size();
+            int n = matrix[0].size();
+            int totalelement = m * n;
     
-             while (count < totalelement){
-                // print starting element ;
-                for ( int i = startcol; i<=endcol; i++ ){
-                           ans.push_back (matrix [startrow][i]) ;
+            int startcol = 0;
+            int endcol = n - 1;
+            int startrow = 0;
+            int endrow = m - 1;
+            int count = 0;
     
-                        }
-                        startrow++;
-                     // end col   
-                  for ( int i = startrow; i<= endrow; i++){
-                           ans.push_back (matrix [i][endcol]) ;
+            while (count < totalelement) {
+                // Traverse from left to right
+                for (int i = startcol; i <= endcol && count < totalelement; i++) {
+                    ans.push_back(matrix[startrow][i]);
+                    count++;
+                }
+                startrow++;
     
-                           
-                        }
-                        endcol--; 
-                        // endrow
-                         for ( int i = endcol; i<=startcol; i--){
-                           ans.push_back (matrix [endrow]][i]) ;
-                           
-                           
-                        }
-                        endrow--;
-                        //startcol
-                         for ( int i = endrow; i<= startrow-1; i--){
-                           ans.push_back (matrix [i][startcol]) ;
-                           
-                           
-                        }
-                        startcol++;
-                         
-             }
+                // Traverse from top to bottom
+                for (int i = startrow; i <= endrow && count < totalelement; i++) {
+                    ans.push_back(matrix[i][endcol]);
+                    count++;
+                }
+                endcol--;
     
+                // Traverse from right to left
+                for (int i = endcol; i >= startcol && count < totalelement; i--) {
+                    ans.push_back(matrix[endrow][i]);
+                    count++;
+                }
+                endrow--;
+    
+                // Traverse from bottom to top
+                for (int i = endrow; i >= startrow && count < totalelement; i--) {
+                    ans.push_back(matrix[i][startcol]);
+                    count++;
+                }
+                startcol++;
+            }
+            return ans;
         }
     };
+    
