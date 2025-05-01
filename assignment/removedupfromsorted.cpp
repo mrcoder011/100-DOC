17d261324f1f5742c30ff12440d83e5cb983bb83
Md Shafatullah <155663678+MRcoder011@users.noreply.github.com>
2025-05-01 22:14:55 +0530

    assignments question

--- assignment/removedupfromsorted.cpp
+++ assignment/removedupfromsorted.cpp
@@ -21,3 +21,21 @@ class Solution {
             
     
     };
+
+
+    // second method
+    class Solution {
+        public:
+            int removeDuplicates(vector<int>& nums) {
+                int curr=nums[0];
+                int index=1;
+                for(int i=1;i<nums.size();i++){
+                    if(curr!=nums[i]){
+                        curr=nums[i];
+                        nums[index]=curr;
+                        index++;
+                    }
+                }
+                return index;
+            }
+        };
