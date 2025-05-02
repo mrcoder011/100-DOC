90b4d8690c7d6b69d3614a615d8f97372a8f9127
Md Shafatullah <155663678+MRcoder011@users.noreply.github.com>
2025-05-03 00:16:55 +0530

    assignments question

---
+++ assignment/maxavgsubarray.cpp
@@ -0,0 +1,19 @@
+class Solution {
+    public:
+        double bruteforce(vector<int>& nums, int k) {
+            double maxAvg = INT_MIN;
+            for (int i = 0; i <= nums.size() - k; ++i) {
+                int sum = 0;
+                for (int j = i; j < i + k; ++j) {
+                    sum += nums[j];
+                }
+                maxAvg = max(maxAvg, sum / (double)k);
+            }
+            return maxAvg;
+        }
+    
+        double findMaxAverage(vector<int>& nums, int k) {
+            return bruteforce(nums, k);
+        }
+         
+    };
