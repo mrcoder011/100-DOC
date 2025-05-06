fb71e3eb0ad564d249589e57c1b3cd568b54fd6c
Md Shafatullah <155663678+MRcoder011@users.noreply.github.com>
2025-05-06 21:25:41 +0530

    assignments question

--- assignment/maxavgsubarray.cpp
+++ assignment/maxavgsubarray.cpp
@@ -17,3 +17,20 @@ class Solution {
         }
          
     }
+
+    // new method
+    double slidingWindow(vector<int>& nums, int k) {
+        double maxAvg = INT_MIN;
+        int sum = 0;
+        for (int i = 0; i < k; ++i) {
+            sum += nums[i];
+        }
+        maxAvg = max(maxAvg, sum / (double)k);
+        
+        for (int i = k; i < nums.size(); ++i) {
+            sum += nums[i] - nums[i - k];
+            maxAvg = max(maxAvg, sum / (double)k);
+        }
+        
+        return maxAvg;
+    }
