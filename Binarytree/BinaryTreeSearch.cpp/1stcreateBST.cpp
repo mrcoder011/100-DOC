c1eb5a0fd921451cd6b0e29236b6cbafcacccad9
Md Shafatullah <155663678+MRcoder011@users.noreply.github.com>
2025-05-31 00:16:33 +0530

    Initial commit - added HTML, CSS, and JavaScript files for EV Range Simulator

--- Binarytree/BinaryTreeSearch.cpp/1stcreateBST.cpp
+++ Binarytree/BinaryTreeSearch.cpp/1stcreateBST.cpp
@@ -1,77 +1,98 @@
-#include <>iostream>
-#include <>queue>
-#include <>vector>
-#include <>map>
-#include <>algorithm>
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <map>
+#include <algorithm>
 
 using namespace std;
-class Node{
-    public:
-    int data 
-    Node*left;
-    Node*right;
-    Node(int val){
+
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
         this->data = val;
         this->left = NULL;
         this->right = NULL;
-        Node * insertBst(Node*root, int data){
-            if(root == NULL){
-                return new Node(data);
-            }
-            if(data > root->data){
-                root->right = insertBst(root->right, data);
-            }else{
-                root->left = insertBst(root->left, data);
-            }
-            return root;
-        }
-    }
-
-    void createBST( Node*&root )
-    {
-        cout << "Enter the data: ";
-        int data;
-        cin >> data;
-        while(data != -1){
-            root = insertBst(root, data);
-            cin >> data;
-
-        }
-        
     }
 };
 
-int main(){
-Node*root = createBST()
-    return 0;
-
+// Function to insert into BST
+Node* insertBst(Node* root, int data) {
+    if (root == NULL) {
+        return new Node(data);
+    }
+    if (data > root->data) {
+        root->right = insertBst(root->right, data);
+    } else {
+        root->left = insertBst(root->left, data);
+    }
+    return root;
 }
-// preorder traversal of the BST
 
+// Function to create BST from input
+void createBST(Node*& root) {
+    cout << "Enter data to insert into BST (-1 to stop): ";
+    int data;
+    cin >> data;
+    while (data != -1) {
+        root = insertBst(root, data);
+        cin >> data;
+    }
+}
+
+// Preorder traversal (NLR)
 void preorder(Node* root) {
     if (root == NULL) return;
     cout << root->data << " ";
     preorder(root->left);
     preorder(root->right);
 }
-// inorder traversal of the BST // LNR
+
+// Inorder traversal (LNR)
 void inorder(Node* root) {
     if (root == NULL) return;
     inorder(root->left);
     cout << root->data << " ";
     inorder(root->right);
 }
-Node*Minvalue(Node* root) {
+
+// Get minimum value in BST
+Node* Minvalue(Node* root) {
     if (root == NULL) return NULL;
     while (root->left != NULL) {
         root = root->left;
     }
     return root;
 }
-Node*Maxvalue(Node* root) {
+
+// Get maximum value in BST
+Node* Maxvalue(Node* root) {
     if (root == NULL) return NULL;
     while (root->right != NULL) {
         root = root->right;
     }
     return root;
+}
+
+int main() {
+    Node* root = NULL;
+    createBST(root);
+
+    cout << "\nPreorder Traversal: ";
+    preorder(root);
+
+    cout << "\nInorder Traversal: ";
+    inorder(root);
+
+    Node* minNode = Minvalue(root);
+    Node* maxNode = Maxvalue(root);
+    if (minNode && maxNode) {
+        cout << "\nMinimum Value in BST: " << minNode->data;
+        cout << "\nMaximum Value in BST: " << maxNode->data;
+    }
+
+    return 0;
 };
